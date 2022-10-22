2f99867f6cbaa410db0e8d1fdbe9b48fa5a0e141
Wan Akid <80897577+Akid0-10@users.noreply.github.com>
2022-10-22 20:55:54 +0800

    update package status patient

--- package_status.php
+++ package_status.php
@@ -69,6 +69,10 @@
                                     else if($row['status'] == "shipped out"){
 
                                         echo "Your package has been shipped out.<br>Kindly wait it to arrived.";
+
+                                        ?>
+                                            <a href="delivery-process.php?username=<?$username?>">Package Arrived</a>
+                                        <?
                                     }
 
                                     //When the status value is other than above e.g('arrived')
